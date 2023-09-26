09be0cb8a3791845d9c600fbbe1db907c44ce1cd
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-09-26 09:36:15 -0700

    Create app that generates the best before date of food.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function AddDaysToDate() {
+  const [startDate, setStartDate] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleStartDateChange = event => {
+    setStartDate(event.target.value);
+  };
+
+  const formatDateToYYMMDD = date => {
+    const year = date.getFullYear().toString().slice(-2);
+    const month = date.toLocaleString("default", { month: "short" });
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year} ${month} ${day}`;
+  };
+
+  const calculateDates = () => {
+    const inputDate = new Date(startDate);
+    if (!isNaN(inputDate.getTime())) {
+      // Create an array of days to calculate
+      const daysToCalculate = [
+        3, 5, 7, 8, 10, 12, 14, 15, 18, 21, 25, 28, 30, 35, 40, 45
+      ];
+
+      // Calculate and format the dates
+      const calculatedDates = daysToCalculate.map(daysToAdd => {
+        const newDate = new Date(inputDate);
+        newDate.setDate(inputDate.getDate() + daysToAdd);
+
+        // Format the date as "YY MM DD"
+        const formattedDate = formatDateToYYMMDD(newDate);
+        return { days: daysToAdd, formattedDate };
+      });
+
+      setResults(calculatedDates);
+    } else {
+      setResults([]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app-container">
+      <div>
+        <h2>Best Before Date Calculator</h2>
+        <label>
+          Enter a start date:
+          <input
+            type="date"
+            value={startDate}
+            onChange={handleStartDateChange}
+          />
+        </label>
+        <button onClick={calculateDates}>Calculate Dates</button>
+        {startDate && (
+          <h4 className="developer">
+            Selected Date: {formatDateToYYMMDD(new Date(startDate))}
+          </h4>
+        )}
+        <div>
+          {results.map(result => (
+            <p key={result.days}>
+              {result.days} days added:&nbsp; &nbsp;
+              {result.formattedDate}
+            </p>
+          ))}
+        </div>
+      </div>
+      <h4 className="developer">
+        Developed by:&nbsp; &nbsp;{" "}
+        <a className="link" href="https://www.cmkcanada.com/">
+          Monica Wolfe
         </a>
-      </header>
+      </h4>
     </div>
   );
 }
 
-export default App;
+export default AddDaysToDate;
